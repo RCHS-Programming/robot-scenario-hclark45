7c527c2644c3e3dd5a9ac8a974af044d801b88df
hclark45 <Rap.GODD@icloud.com>
2021-04-01 12:30:59 -0400

    Added world and robot movement

--- Robot.java
+++ Robot.java
@@ -8,12 +8,47 @@ import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
  */
 public class Robot extends Actor
 {
-    /**
-     * Act - do whatever the Robot wants to do. This method is called whenever
+   /**
+    * Act - do whatever the Robot wants to do. This method is called whenever
      * the 'Act' or 'Run' button gets pressed in the environment.
-     */
-    public void act() 
+    */
+   public void Act ()
     {
-        // Add your action code here.
+        robotMovement();
+        detectWallCollision();
+        
+    }
+    public void robotMovement() 
+    {
+      if (Greenfoot.isKeyDown("up"))
+      { 
+          setLocation( getX(), getY() -3 );
+      }
+       if (Greenfoot.isKeyDown("down"))
+      { 
+          setLocation( getX(), getY() +3 ); 
+      }
+       if (Greenfoot.isKeyDown("right"))
+      { 
+          setLocation( getY(), getX() +3 );
+      }
+       if (Greenfoot.isKeyDown("left"))
+      { 
+          setLocation( getY(), getX() -3 );
+      }
     }    
+    public void detectWallCollision()
+    {
+        if (isTouching(Wall.class))
+        {
+            setLocation(50,85);
+        }
+    }
+    public void detectBlockCollision()
+    {
+        if (isTouching(Wall.class))
+        {
+            setLocation(50,85);
+        }
+    }
 }
--- RobotWorld.java
+++ RobotWorld.java
@@ -18,7 +18,31 @@ public class RobotWorld extends World
     {    
         // Create a new world with 600x400 cells with a cell size of 1x1 pixels.
         super(800, 600, 1); 
-        addObject(new Wall(),66,173);
+        addObject(new Wall(),55,173);
+        addObject(new Wall(),90,173);
+        addObject(new Wall(),125,173);
+        addObject(new Wall(),160,173);
+        addObject(new Wall(),195,173);
+        addObject(new Wall(),230,173);
+        addObject(new Wall(),265,173);
+        addObject(new Wall(),765,173);
+        addObject(new Wall(),730,173);
+        addObject(new Wall(),695,173);
+        addObject(new Wall(),660,173);
+        addObject(new Wall(),625,173);
+        addObject(new Wall(),590,173);
+        addObject(new Robot(),50,85);
+        addObject(new Block(),430,173);
+        addObject(new Pizza(),265,85);
+        addObject(new Pizza(),625,85);
+        addObject(new Pizza(),65,285);
+        addObject(new Pizza(),430,325);
+        addObject(new Pizza(),765,285);
+        addObject(new Pizza(),265,525);
+        addObject(new Pizza(),625,525);
+        addObject(new Home(),740,545);
+        
+       
     }
     
 }
